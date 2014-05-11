a1aca3535ddbfa8f79e360d1923ed82982f524a8
gilarthon <comporell@gmail.com>
2014-05-11 14:16:31 +0000

    Master

--- ControlPad.js
+++ ControlPad.js
@@ -9,34 +9,67 @@ var t = require('./lib/tank.js');
 var app = require('http').createServer(handler);
 var io = require('socket.io').listen(app);
 var fs= require('fs');
+var path = require('path');
 
-function handler(req,res){
- fs.readFile('index.html',
-    function(err,data){
-        if(err){
-            res.writeHead(500);
-            return res.end('500.html');
-        }
-        
-        res.writeHead(200);
-        res.end(data);
-    });
+app.listen(9090);
+
+function handler(request,response){
+
+	var filePath = '.' + request.url;
+	if (filePath == './')
+		filePath = './index.html';
+		
+	var extname = path.extname(filePath);
+	var contentType = 'text/html';
+	switch (extname) {
+		case '.js':
+			contentType = 'text/javascript';
+			break;
+		case '.css':
+			contentType = 'text/css';
+			break;
+	}
+	
+	path.exists(filePath, function(exists) {
+	
+		if (exists) {
+			fs.readFile(filePath, function(error, content) {
+				if (error) {
+					response.writeHead(500);
+					response.end();
+				}
+				else {
+					response.writeHead(200, { 'Content-Type': contentType });
+					response.end(content, 'utf-8');
+				}
+			});
+		}
+		else {
+			response.writeHead(404);
+			response.end();
+		}
+	});
+    
+    
 }
 
 io.sockets.on('connection',function(socket){
-    socket.on('sendval',function(data){
+    socket.on('moveRelative',function(data){
         console.log("Socket :" + socket);
         console.log("Request :" + data);
-        control(data.toLowerCase());
+        t.moveRelative(data.direction,data.duration);
+
+        
+        //t.moveRelative(data.toLowerCase(),3000);
+        
+        
         //socket.emit('datastatus','sent');
         //socket.broadcast.emit('dataupdate','ok);
     });
 });
+console.log("System ON");
 
-
-t.moveRelative("forward",3000);
-
-
+t.resetSystem();
 
 //function control(d1){
 //    t.moveRelative(d1,3000);
